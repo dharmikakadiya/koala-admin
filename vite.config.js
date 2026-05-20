0381a97834c8f14431a406b2807b3c2e981f58c8
dharmikakadiya <dharmikakadiya2005@gmail.com>
2026-05-20 14:55:55 +0530

    add vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
+  resolve: {
+    alias: {
+      "@": "/src", // ✅ optional shortcut
+    },
+  },
+  base: '/',
+  server: {
+    host: true, // or '0.0.0.0'
+    port: 5173,
+    allowedHosts: true
+  },
+  
 })
